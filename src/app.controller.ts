3099042ea896239139750c31b184f91111a524b5
Rafael Aparecido <rafaap2003@gmail.com>
2022-12-22 17:07:24 -0300

    feat: finishing update and delete endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ import {
   Param,
   Post,
 } from '@nestjs/common';
-import { Body, Delete, Put } from '@nestjs/common/decorators';
+import { Body, Delete, HttpCode, Put } from '@nestjs/common/decorators';
 import { data, ReportType } from './data';
 import { randomUUID } from 'node:crypto';
 
@@ -16,6 +16,12 @@ export class AppController {
 
   @Get()
   getAllIncomeReports(@Param('type') type: string) {
+    if (type !== 'income' && type !== 'expense') {
+      throw new HttpException(
+        `This ${type} endpoint not supported.`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
     return data.report.filter((report) => report.type === type);
   }
 
@@ -56,12 +62,7 @@ export class AppController {
   }
 
   @Put(':id')
-  updateIncomeReport(@Param('id') id: string, @Param('type') type: string) {
-    return [id, type];
-  }
-
-  @Delete(':id')
-  deleteIncomeReport(
+  updateIncomeReport(
     @Param('id') id: string,
     @Param('type') type: string,
     @Body() { source, amount }: any,
@@ -69,6 +70,42 @@ export class AppController {
     const report = data.report
       .filter((report) => report.type === type)
       .find((report) => report.id === id);
-    return [id, type];
+
+    if (!report) {
+      throw new HttpException(
+        `You haven't a ${type} with this id : ${id}`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const index = data.report.findIndex((report) => report.id === report.id);
+
+    data.report[index] = {
+      id: report.id,
+      amount: amount ?? report.amount,
+      source: source ?? report.source,
+      type: report.type,
+      created_at: report.created_at,
+      updated_at: new Date(),
+    };
+
+    return data.report[index];
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  deleteIncomeReport(@Param('id') id: string) {
+    const reportToDeleteIndex = data.report.findIndex(
+      (report) => report.id === id,
+    );
+
+    if (reportToDeleteIndex === -1) {
+      throw new HttpException(
+        `Report with this id : ${id} not found.`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    data.report.splice(reportToDeleteIndex, 1);
   }
 }
