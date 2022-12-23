3e05c5281877982020eb2f11e1461af9d3e8f9e9
Rafael Aparecido <rafaap2003@gmail.com>
2022-12-23 14:46:05 -0300

    feat: abstracting the business logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,43 +1,19 @@
-import {
-  Controller,
-  Get,
-  HttpException,
-  HttpStatus,
-  Param,
-  Post,
-} from '@nestjs/common';
+import { AppService } from './app.service';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { Body, Delete, HttpCode, Put } from '@nestjs/common/decorators';
-import { data, ReportType } from './data';
-import { randomUUID } from 'node:crypto';
 
 @Controller('report/:type')
 export class AppController {
-  // constructor() {}
+  constructor(private readonly appservice: AppService) {}
 
   @Get()
   getAllIncomeReports(@Param('type') type: string) {
-    if (type !== 'income' && type !== 'expense') {
-      throw new HttpException(
-        `This ${type} endpoint not supported.`,
-        HttpStatus.NOT_FOUND,
-      );
-    }
-    return data.report.filter((report) => report.type === type);
+    return this.appservice.getAllIncomeReports(type);
   }
 
   @Get(':id')
   getIncomeReportById(@Param('id') id: string, @Param('type') type: string) {
-    const report = data.report
-      .filter((report) => report.type === type)
-      .find((report) => report.id === id);
-
-    if (!report)
-      throw new HttpException(
-        `You haven't a ${type} with this id : ${id}`,
-        HttpStatus.NOT_FOUND,
-      );
-
-    return report;
+    return this.appservice.getIncomeReportById(id, type);
   }
 
   @Post()
@@ -45,20 +21,7 @@ export class AppController {
     @Body() { source, amount }: any,
     @Param('type') type: string,
   ) {
-    const typeReport =
-      type === ReportType.INCOME ? ReportType.INCOME : ReportType.EXPENSE;
-    const newReport = {
-      id: randomUUID(),
-      source,
-      amount,
-      type: typeReport,
-      created_at: new Date(),
-      updated_at: new Date(),
-    };
-
-    data.report.push(newReport);
-
-    return newReport;
+    return this.appservice.createIncomeReport({ source, amount }, type);
   }
 
   @Put(':id')
@@ -67,45 +30,12 @@ export class AppController {
     @Param('type') type: string,
     @Body() { source, amount }: any,
   ) {
-    const report = data.report
-      .filter((report) => report.type === type)
-      .find((report) => report.id === id);
-
-    if (!report) {
-      throw new HttpException(
-        `You haven't a ${type} with this id : ${id}`,
-        HttpStatus.NOT_FOUND,
-      );
-    }
-
-    const index = data.report.findIndex((report) => report.id === report.id);
-
-    data.report[index] = {
-      id: report.id,
-      amount: amount ?? report.amount,
-      source: source ?? report.source,
-      type: report.type,
-      created_at: report.created_at,
-      updated_at: new Date(),
-    };
-
-    return data.report[index];
+    return this.appservice.updateIncomeReport(id, type, { source, amount });
   }
 
   @HttpCode(204)
   @Delete(':id')
   deleteIncomeReport(@Param('id') id: string) {
-    const reportToDeleteIndex = data.report.findIndex(
-      (report) => report.id === id,
-    );
-
-    if (reportToDeleteIndex === -1) {
-      throw new HttpException(
-        `Report with this id : ${id} not found.`,
-        HttpStatus.NOT_FOUND,
-      );
-    }
-
-    data.report.splice(reportToDeleteIndex, 1);
+    this.appservice.deleteIncomeReport(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
+import { data, ReportType } from './data';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAllIncomeReports(type: string) {
+    const typeReport =
+      type === ReportType.INCOME ? ReportType.INCOME : ReportType.EXPENSE;
+    if (type !== 'income' && type !== 'expense') {
+      throw new HttpException(
+        `This ${type} endpoint not supported.`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return data.report.filter((report) => report.type === typeReport);
+  }
+
+  getIncomeReportById(id: string, type: string) {
+    const report = data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id);
+
+    if (!report)
+      throw new HttpException(
+        `You haven't a ${type} with this id : ${id}`,
+        HttpStatus.NOT_FOUND,
+      );
+
+    return report;
+  }
+
+  createIncomeReport(body: any, type: string) {
+    const typeReport =
+      type === ReportType.INCOME ? ReportType.INCOME : ReportType.EXPENSE;
+    const newReport = {
+      id: randomUUID(),
+      source: body.source,
+      amount: body.amount,
+      type: typeReport,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    data.report.push(newReport);
+
+    return newReport;
+  }
+
+  updateIncomeReport(id: string, type: string, body: any) {
+    const report = data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id);
+
+    if (!report) {
+      throw new HttpException(
+        `You haven't a ${type} with this id : ${id}`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const index = data.report.findIndex((report) => report.id === report.id);
+
+    data.report[index] = {
+      id: report.id,
+      amount: body.amount ?? report.amount,
+      source: body.source ?? report.source,
+      type: report.type,
+      created_at: report.created_at,
+      updated_at: new Date(),
+    };
+
+    return data.report[index];
+  }
+
+  deleteIncomeReport(id: string) {
+    const reportToDeleteIndex = data.report.findIndex(
+      (report) => report.id === id,
+    );
+
+    if (reportToDeleteIndex === -1) {
+      throw new HttpException(
+        `Report with this id : ${id} not found.`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    data.report.splice(reportToDeleteIndex, 1);
   }
 }
